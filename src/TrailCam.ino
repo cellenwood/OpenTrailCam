1578a0c1b9d49f63bccb0a83dab98257a65dfc93
Chris Ellenwood <fun2xs2004@yahoo.com>
2020-01-04 14:08:05 -0600

    Moved brownout detection to motion sensor

--- src/TrailCam.ino
+++ src/TrailCam.ino
@@ -1,6 +1,4 @@
 #include "Arduino.h"
-#include "soc/soc.h"           // Disable brownout problems
-#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
 #include "driver/rtc_io.h"
 #include "MotionSensor.h"
 #include "Camera.h"
@@ -16,14 +14,10 @@ Storage storage;
 #define uS_TO_S_FACTOR 1000000
   
 void setup() {
-  DisableBrownOutDetector();
   InitializeSerial(115200);
+  motionSensor.Initialize();
   camera.Initialize();
   storage.Initialize();
-} 
-
-void DisableBrownOutDetector(){
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
 }
 
 void InitializeSerial(unsigned long speed) {
